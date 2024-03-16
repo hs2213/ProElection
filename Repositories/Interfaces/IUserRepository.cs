60b5a1948f28b266e415fd34f18b55ac9c89ea83
Hamzah <hsidat@redev.co.uk>
2024-03-16 23:18:33 +0000

    Added features to UserRepository.cs ✨

--- Repositories/Interfaces/IUserRepository.cs
+++ Repositories/Interfaces/IUserRepository.cs
@@ -1,6 +1,35 @@
-﻿namespace ProElection.Repositories.Interfaces;
+﻿using ProElection.Entities;
+using ProElection.Entities.Enums;
+
+namespace ProElection.Repositories.Interfaces;
 
 public interface IUserRepository
 {
-    
+    /// <summary>
+    /// Gets a user from the database by its Id.
+    /// </summary>
+    /// <param name="id"><see cref="Guid"/> Id of user to get.</param>
+    /// <returns><see cref="User"/> with the given ID or null if not found.</returns>
+    public Task<User?> GetUserByIdAsync(Guid id);
+
+    /// <summary>
+    /// Gets a user from the database by its email and hashed password.
+    /// </summary>
+    /// <param name="email"><see cref="string"/> containing the user's email</param>
+    /// <param name="hashedPassword"><see cref="string"/> containing the user's hashed password.</param>
+    /// <returns><see cref="User"/> with the given email and password or null if not found.</returns>
+    public Task<User?> GetUserByEmailAndPassword(string email, string hashedPassword);
+
+    /// <summary>
+    /// Creates a new user in the database.
+    /// </summary>
+    /// <param name="user"><see cref="User"/> to add to the database.</param>
+    /// <returns><see cref="User"/> added to the database</returns>
+    public Task<User> CreateUserAsync(User user);
+
+    /// <summary>
+    /// Gets all the users with the type of <see cref="UserType.Candidate"/>
+    /// </summary>
+    /// <returns>List of candidates in the database.</returns>
+    public IEnumerable<User> GetCandidatesAsync();
 }
--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -1,4 +1,7 @@
-﻿using ProElection.Persistence;
+﻿using Microsoft.EntityFrameworkCore;
+using ProElection.Entities;
+using ProElection.Entities.Enums;
+using ProElection.Persistence;
 using ProElection.Repositories.Interfaces;
 
 namespace ProElection.Repositories;
@@ -10,5 +13,32 @@ public class UserRepository : IUserRepository
     public UserRepository(ProElectionDbContext dbContext)
     {
         _dbContext = dbContext;
+    }
+    
+    /// <inheritdoc/>
+    public async Task<User?> GetUserByIdAsync(Guid id)
+    {
+        return await _dbContext.Users.SingleOrDefaultAsync(user => user.Id == id);
+    }
+    
+    /// <inheritdoc/>
+    public async Task<User?> GetUserByEmailAndPassword(string email, string hashedPassword)
+    {
+        return await _dbContext.Users
+            .SingleOrDefaultAsync(user => user.Email == email && user.HashedPassword == hashedPassword);
+    }
+    
+    /// <inheritdoc/>
+    public async Task<User> CreateUserAsync(User user)
+    {
+        await _dbContext.Users.AddAsync(user);
+        await _dbContext.SaveChangesAsync();
+        return user;
+    }
+    
+    /// <inheritdoc/>
+    public IEnumerable<User> GetCandidatesAsync()
+    {
+        return _dbContext.Users.Where(user => user.UserType == UserType.Candidate);
     }
 }
